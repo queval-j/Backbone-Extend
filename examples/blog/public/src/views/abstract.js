1f259d78842a56584363682c9fe3cc97062824d3
Jean Christophe Queval-Bourgeois <jean-christophe.queval@epitech.eu>
2014-03-29 16:32:41 +0100

    remove abstract.js from the example workshop 3

--- examples/blog/public/src/views/abstract.js
+++
@@ -1,9 +0,0 @@
-app.Views.Abstract = Backbone.Page.extend({
-	initForAll: function () {
-		this.e.on('show', this._mainShow, this);
-	},
-	_mainShow: function () {
-		this.app.getMenu().removeClass('active');
-		$('li[data-id="'+this._dataId+'"]', this.app.getMenu().parent()).addClass('active');
-	}
-});
